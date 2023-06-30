556fc145ddef84ef3631a41d9ff22353efdd5a96
Cristi Sandu <sandu.cristi@yahoo.com>
2023-06-30 15:35:16 +0300

    Add project files.

---
+++ lab1ex7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Program care va verifica daca un numar citit de la tastatura este par sau impar. In cazul in care numarul este par, programul va afisa “Numar par” iar in caz contrar, “Numar impar” */
+
+            int n1;
+            
+            Console.WriteLine("Introduceti numarul:" + "\n");
+            n1 = int.Parse(Console.ReadLine());
+
+        
+            if (n1%2==0)
+            {
+                Console.WriteLine("\n" + "Numar par.");
+            }
+
+
+            else
+            {
+                Console.WriteLine("\n" + "Numar impar.");
+            }
+
+
+        }
+    }
+}
